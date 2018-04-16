3331ecf5ed5dc482ba3414e659020596ae0fc14e
Rahul Rajabhoj <rahulrajabhoj@gmail.com>
2018-04-16 16:24:59 +0530

    Used AIRbnb tslint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { ErrorHandler, NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { SplashScreen } from "@ionic-native/splash-screen";
-import { StatusBar } from "@ionic-native/status-bar";
-import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
+import { ErrorHandler, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
-import { HomePage } from "../pages/home/home";
-import { MyApp } from "./app.component";
+import { HomePage } from '../pages/home/home';
+import { MyApp } from './app.component';
 
 @NgModule({
   bootstrap: [IonicApp],
@@ -24,7 +24,7 @@ import { MyApp } from "./app.component";
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
   ],
 })
 export class AppModule {}
