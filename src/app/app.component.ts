2addfe4b68e0d259fdad15c991fb073426186057
Rahul Rajabhoj <rahulrajabhoj@gmail.com>
2018-04-16 17:41:04 +0530

    Updated rules in tslint.json file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from "@angular/core";
-import { SplashScreen } from "@ionic-native/splash-screen";
-import { StatusBar } from "@ionic-native/status-bar";
-import { Platform } from "ionic-angular";
+import { Component } from '@angular/core';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { Platform } from 'ionic-angular';
 
-import { HomePage } from "../pages/home/home";
+import { HomePage } from '../pages/home/home';
 @Component({
-  templateUrl: "app.html",
+  templateUrl: 'app.html',
 })
 export class MyApp {
   public rootPage: any = HomePage;
